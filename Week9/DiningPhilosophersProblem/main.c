40ac0b43a2d76220b00231375e76049767efd20a
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2022-05-18 18:58:37 +0200

    pThread

--- Week9/DiningPhilosophersProblem/main.c
+++ Week9/DiningPhilosophersProblem/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <windows.h>
-#include "OSCO.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+// #include "OSCO.h"
 
 /*Philosophers Diner
 * ------------------
@@ -16,78 +18,83 @@
 *  can immediately grab boh forks and sets up the numEating semaphors to allow (N-1)
 *  philsophers to try to grab their forks and start eating.
 */
-HANDLE forks[NUM_DINERS];
-HANDLE numEating;
-DWORD WINAPI Philosopher(LPVOID lpParam);
-CRITICAL_SECTION criticalSection;
+sem_t forks[NUM_DINERS];
+sem_t numEating;
+void* Philosopher(void* lpParam);
+// CRITICAL_SECTION criticalSection;
 
 
 int main(int argc, char** argv)
 {
-	HANDLE philosopherThreads[NUM_DINERS];
-	InitializeCriticalSection(&criticalSection);
+	pthread_t philosopherThreads[NUM_DINERS];
+	// InitializeCriticalSection(&criticalSection);
 
 	printf("Main started. %d philosophers that eat %d times.\n", NUM_DINERS, EAT_TIMES);
 
 	//Create the forks
-	numEating = CreateSemaphore(NULL, 0, NUM_DINERS, NULL);
+	sem_init(&numEating, 0, 2);
 	for (int i = 0; i < NUM_DINERS; i++) {
-		forks[i] = CreateSemaphore(NULL, 0, 1, NULL);
+		sem_init(&forks[i], 0, 1);
 	}
 
 	//Create the philosophers and start them up
 	int a[NUM_DINERS];
 	for (int i = 0; i < NUM_DINERS; i++) {
 		a[i] = i;
-		philosopherThreads[i] = CreateThread(NULL, 0, Philosopher, &a[i], 0, NULL);
+		pthread_create(&philosopherThreads[i], NULL, Philosopher, &a[i]);
+		// philosopherThreads[i] = CreateThread(NULL, 0, Philosopher, &a[i], 0, NULL);
 
-		if (philosopherThreads[i] == NULL)
-		{
-			printf("Error creating philosopher %d\n", i);
-			ExitProcess(3);
-		}
+		// if (philosopherThreads[i] == NULL)
+		// {
+		// 	printf("Error creating philosopher %d\n", i);
+		// 	ExitProcess(3);
+		// }
 	}
-
-	SetThreadPriority(philosopherThreads[3], THREAD_PRIORITY_HIGHEST);
 
 
 	//Wait for all threads to finish
-	WaitForMultipleObjects(NUM_DINERS, philosopherThreads, TRUE, 0xFFFFFFFF);
+	for (int i = 0; i < NUM_DINERS; i++) 
+	{
+		pthread_join(philosopherThreads[i], NULL);
+	}
 
-	CloseHandle(numEating);
+	sem_destroy(&numEating);
 	for (int i = 0; i < NUM_DINERS; i++) {
-		CloseHandle(forks[i]);
+		sem_destroy(&forks[i]);
 	}
 	printf("All done!\n");
 }
 
-DWORD WINAPI Philosopher(LPVOID lpParam) {
+void* Philosopher(void* lpParam) {
 	int philosopherNo = *(int*)lpParam;
 	int timesAte = 0;
-	DWORD waitResultR;
-	DWORD waitResultL;
 
-	WaitForSingleObject(numEating, 0L);
-	EnterCriticalSection(&criticalSection);
+	// WaitForSingleObject(numEating, 0L);
+	// EnterCriticalSection(&criticalSection);
 	printf("Philosopher %d is thinking...\n", philosopherNo);
-	LeaveCriticalSection(&criticalSection);
-	Sleep(rand_between(250, 1000));
+	// LeaveCriticalSection(&criticalSection);
+	// Sleep(rand_between(250, 1000));
+	sleep(1);
 	while (timesAte < EAT_TIMES) {
-		waitResultR = WaitForSingleObject(forks[(philosopherNo + 1) % 5], 0L);
-		waitResultL = WaitForSingleObject(forks[philosopherNo], 0L);
+		// waitResultR = WaitForSingleObject(forks[(philosopherNo + 1) % 5], 0L);
+		// waitResultL = WaitForSingleObject(forks[philosopherNo], 0L);
+		sem_wait(&forks[(philosopherNo + 1) % 5]);
+		sem_wait(&forks[philosopherNo]);
 
-		EnterCriticalSection(&criticalSection);
-		printf("Philosopher %d: Starting to eat for %d. time\n", philosopherNo, timesAte + 1);
-		LeaveCriticalSection(&criticalSection);
-		Sleep(1000);
-		EnterCriticalSection(&criticalSection);
-		printf("Philosopher %d: Finished eating for %d. time\n", philosopherNo, timesAte + 1);
-		LeaveCriticalSection(&criticalSection);
+		// EnterCriticalSection(&criticalSection);
+		printf("Philosopher %d: STARTING to eat for %d. time\n", philosopherNo, timesAte + 1);
+		// LeaveCriticalSection(&criticalSection);
+		sleep(1);
+		// EnterCriticalSection(&criticalSection);
+		printf("Philosopher %d: finished eating for %d. time\n", philosopherNo, timesAte + 1);
+		// LeaveCriticalSection(&criticalSection);
 		timesAte++;
 
 		//Done eating.. releasing semaphores
-		ReleaseSemaphore(numEating, 1, NULL);
-		ReleaseSemaphore(forks[philosopherNo], 1, NULL);
-		ReleaseSemaphore(forks[(philosopherNo + 1) % 5], 1, NULL);
+		// ReleaseSemaphore(numEating, 1, NULL);
+		// ReleaseSemaphore(forks[philosopherNo], 1, NULL);
+		// ReleaseSemaphore(forks[(philosopherNo + 1) % 5], 1, NULL);
+		sem_post(&forks[(philosopherNo + 1) % 5]);
+		sem_post(&forks[philosopherNo]);
 	}
 }
